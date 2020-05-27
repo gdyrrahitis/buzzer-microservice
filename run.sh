3480246c22bdb6c5846a2900461ba6abe7df94c6
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2020-05-27 02:07:39 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
-    echo "$(date) - waiting for rabbitmq..."
-    sleep 2
-done
+if [ "$APP_ENV" != "production" ]; then
+    until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
+        echo "$(date) - waiting for rabbitmq..."
+        sleep 2
+    done
+fi
 
 # run microservice
 nameko run --config config.yml buzzer.service --backdoor 3000
